b6e14212dba5e31a54a46a55d3ec5153b632fa77
mahmoudrizk <mahmoudahmedrizk@gmail.com>
2024-07-15 00:24:12 +0300

    feat: implement base controller.

---
+++ app/controllers/base_controller.rb
@@ -0,0 +1,16 @@
+class BaseController < ActionController::API
+    def respond_success(message, data)
+        respond(200, message, data)
+    end
+
+    def respond_not_found(message)
+        respond(401, message, {})
+    end
+
+    def respond(status_code, message, data)
+        render json: {
+            message: message,
+            data: data
+        }, status: status_code
+    end
+end
