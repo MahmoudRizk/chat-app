57aa8da6d2332d96739152ddb4afa63cdaf5f072
mahmoudrizk <mahmoudahmedrizk@gmail.com>
2024-07-17 22:52:52 +0300

    feat: count messages per chat using the naive way of auto incrementing after each insert.

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,6 @@
 class Message < ApplicationRecord
   before_save :set_count_in_chat
+  after_save  :update_messages_count_for_chat
 
   belongs_to :chat
 
@@ -11,6 +12,12 @@ class Message < ApplicationRecord
   def set_count_in_chat
     max_count_in_chat = Message.where(chat_id: self.chat_id).maximum(:count_in_chat) || 0
     self.count_in_chat = max_count_in_chat + 1
+  end
+
+  def update_messages_count_for_chat
+    chat = Chat.find(self.chat_id)
+    chat.messages_count += 1
+    chat.save
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_17_192501) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_17_194323) do
   create_table "applications", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "uuid"
     t.string "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_17_192501) do
     t.integer "count_in_application", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "messages_count", default: 0
     t.index ["application_id"], name: "index_chats_on_application_id"
   end
 
